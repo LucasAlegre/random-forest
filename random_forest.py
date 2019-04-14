127f211a3e1bc5e5b53e299c81561d50b123bad7
Lucas Alegre <lucasnale@gmail.com>
2019-04-14 00:33:27 -0300

    RandomTree with Categorical Attributes

--- random_forest.py
+++ random_forest.py
@@ -1,19 +1,25 @@
 import pandas as pd
 from random_forest.random_forest import RandomForest
+from random_forest.random_tree import RandomTree
 
 
 if __name__ == '__main__':
 
-    #df = pd.read_csv('datasets/dadosBenchmark_validacaoAlgoritmoAD.csv', sep=';')
-    #print(df, df.iloc[1])
-
+    df = pd.read_csv('datasets/dadosBenchmark_validacaoAlgoritmoAD.csv', sep=';')
+    class_column = 'Joga'
+    
     #df = pd.read_csv('datasets/ionosphere.csv')
+    #class_column = 'g/b'
     #print(df)
 
     #df = pd.read_csv('datasets/wine.csv')
+    #class_column = 'class'
     #print(df)
 
     #df = pd.read_csv('datasets/wdbc.csv')
+    #class_column = 'diagnosis'
     #print(df)
 
-    forest = RandomForest()
+    tree = RandomTree(df, class_column)
+    tree.print_tree()
+
--- random_forest/random_tree.py
+++ random_forest/random_tree.py
@@ -1,6 +1,97 @@
+from math import log2
 
 
 class RandomTree:
 
-    def __init__(self):
-        pass
+    AttributeMetadata = None
+
+    def __init__(self, data, class_column):
+        self.data = data
+        self.class_column = class_column
+        self.attributes = set(self.data.columns.values)
+        self.attributes.remove(self.class_column)  # all columns except the class
+        RandomTree.AttributeMetadata = {atr: data[atr].unique().tolist() for atr in self.attributes}
+        self.root = RandomTreeNode(data, self.attributes.copy(), self.class_column)
+       
+    def predict(self, instance):
+        return self.root.predict(instance)
+
+    def print_tree(self):
+        self.root.print_node()
+
+class RandomTreeNode:
+
+    def __init__(self, data, attributes, class_column, is_leaf=False, terminal_class=None):
+        self.is_leaf = is_leaf
+        self.is_numerical = None
+        self.class_column = class_column
+        self.cut_point = None
+        self.terminal_class = terminal_class
+        self.children = dict()
+
+        if self._number_of_classes(data) == 1:
+            self.is_leaf = True
+            self.terminal_class = data[class_column].values[0]
+            return
+        
+        if len(attributes) == 0:
+            self.is_leaf = True
+            self.terminal_class = data[class_column].value_counts().idxmax()
+            return
+
+        entropy = self.entropy(data)
+        self.node_attribute = max(attributes, key=lambda a: entropy - self.entropy_attribute(data, a))
+        self.is_numerical = data[self.node_attribute].dtype != object
+        attributes.remove(self.node_attribute)
+
+        for v in RandomTree.AttributeMetadata[self.node_attribute]:
+            dv = data[data[self.node_attribute] == v]
+            if len(dv) == 0:
+                self.is_leaf = True
+                self.terminal_class = data[class_column].value_counts().idxmax()
+                return
+            else:
+                self.children[v] = RandomTreeNode(dv, attributes.copy(), class_column)
+
+    def predict(self, instance):
+        if self.is_leaf:
+            return self.terminal_class
+        if self.is_numerical:
+            return self.children[instance[self.node_attribute] < self.cut_point].predict(instance)
+        
+        return self.children[instance[self.node_attribute]].predict(instance)
+
+    def _number_of_classes(self, data):
+        return data[self.class_column].nunique()
+
+    def entropy(self, data):
+        n = len(data)
+        value_counts = data[self.class_column].value_counts().tolist()
+        return sum((-x/n)*log2(x/n) for x in value_counts)
+    
+    def entropy_attribute(self, data, attribute):
+        n = len(data)
+        mean_entropy = 0
+
+        if data[attribute].dtype == object:  # categorical attribute (object == str on Pandas)
+            for _, g in data.groupby(attribute):
+                g_size = len(g)
+                value_counts = g[self.class_column].value_counts().tolist()
+                entropy = sum((-x/g_size)*log2(x/g_size) for x in value_counts)
+                mean_entropy += (g_size/n)*entropy
+        
+        else:  # numerical attribute
+            pass
+        
+        return mean_entropy
+    
+    def print_node(self, height=0):
+        if self.is_leaf:
+            print('{} {}'.format(height*'--', self.terminal_class))
+        else:
+            print('{} {}'.format(height*'--', self.node_attribute))
+            for c in self.children.values():
+                c.print_node(height + 1)
+
+    
+        
