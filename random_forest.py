e8ac9059cb218ef3e56af60403bc2ed00981fafe
Bruno <bsmlima@outlook.com>
2019-05-01 17:16:25 -0300

    Readable arguments and dataframe shuffle

--- random_forest.py
+++ random_forest.py
@@ -13,17 +13,17 @@ if __name__ == '__main__':
     prs = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter,
                                   description="Random Forest - Aprendizado de Máquina 2019/1 UFRGS")
 
-    prs.add_argument("-s",    dest="seed",         required=False, default=None, type=int, help="The random seed.\n")
+    prs.add_argument("-s",    dest="seed",         required=False, default=None,                help="The random seed.\n", type=int)
     prs.add_argument("-d",    dest="data",         required=False, default='datasets/wdbc.csv', help="The dataset .csv file.\n")
-    prs.add_argument("-c",    dest="class_column", required=False, default='diagnosis', help="The column of the .csv to be predicted.\n")
-    prs.add_argument("-sep",  dest="sep",          required=False, default=',',  help=".csv separator.\n")
-    prs.add_argument("-n",    dest="num_trees",    required=False, default=5,    type=int,help="The number of trees in the random forest.\n")
-    prs.add_argument("-k",    dest="num_folds",    required=False, default=10,   type=int, help="The number of folds used on cross validation.\n")
-    prs.add_argument('-drop', nargs='+',           required=False, default=[],   help="Columns to drop from .csv.")
+    prs.add_argument("-c",    dest="class_column", required=False, default='diagnosis',         help="The column of the .csv to be predicted.\n")
+    prs.add_argument("-sep",  dest="sep",          required=False, default=',',                 help=".csv separator.\n")
+    prs.add_argument("-n",    dest="num_trees",    required=False, default=5,                   help="The number of trees in the random forest.\n", type=int)
+    prs.add_argument("-k",    dest="num_folds",    required=False, default=10,                  help="The number of folds used on cross validation.\n", type=int)
+    prs.add_argument('-drop', nargs='+',           required=False, default=[],                  help="Columns to drop from .csv.")
     
-    prs.add_argument("-not-sample",  action='store_true', default=False, required=False, help="Do not sample attributes on each node.\n")
-    prs.add_argument("-cut-by-mean", action='store_true', default=False, required=False, help="Cut point by mean of numerical attribute.\n")
-    prs.add_argument("-v",           action='store_true', default=False, required=False, help="View random tree image.\n")
+    prs.add_argument("-not-sample",  action='store_true', required=False, default=False, help="Do not sample attributes on each node.\n")
+    prs.add_argument("-cut-by-mean", action='store_true', required=False, default=False, help="Cut point by mean of numerical attribute.\n")
+    prs.add_argument("-v",           action='store_true', required=False, default=False, help="View random tree image.\n")
 
     args = prs.parse_args()
 
@@ -31,6 +31,8 @@ if __name__ == '__main__':
     np.random.seed(args.seed)
 
     df = pd.read_csv(args.data, sep=args.sep)
+    df = df.sample(frac=1).reset_index(drop=True) # Shuffle DataFrame rows
+
     class_column = args.class_column
 
     for column in args.drop:
